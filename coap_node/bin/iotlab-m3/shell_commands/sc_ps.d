bcfceed8d462838ebaa74a4842f74ccb9c88199c
Toni Enberg <tenberg21@student.oulu.fi>
2024-01-30 19:31:39 +0200

    All

---
+++ coap_node/bin/iotlab-m3/shell_commands/sc_ps.d
@@ -0,0 +1,8 @@
+/senslab/users/iot2023oulu49/mini_project_1/code/bin/iotlab-m3/shell_commands/sc_ps.o: \
+ /senslab/users/iot2023oulu49/RIOT/sys/shell/commands/sc_ps.c \
+ /senslab/users/iot2023oulu49/mini_project_1/code/bin/iotlab-m3/riotbuild/riotbuild.h \
+ /senslab/users/iot2023oulu49/RIOT/sys/include/ps.h
+
+/senslab/users/iot2023oulu49/mini_project_1/code/bin/iotlab-m3/riotbuild/riotbuild.h:
+
+/senslab/users/iot2023oulu49/RIOT/sys/include/ps.h:
